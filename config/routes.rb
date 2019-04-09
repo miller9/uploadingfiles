99755e2655dbf5d6e36fe34cc5237787829769cd
miller9 <millerpoli9@gmail.com>
2019-04-08 20:22:00 -0500

    Updating routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
-Rails.application.routes.draw do
-  get 'resumes/index'
-  get 'resumes/new'
-  get 'resumes/create'
-  get 'resumes/destroy'
+#Rails.application.routes.draw do
+#  get 'resumes/index'
+#  get 'resumes/new'
+#  get 'resumes/create'
+#  get 'resumes/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+CarrierWaveExample::Application.routes.draw do
+
+	resources :resumes, only: [:index, :new, :create, :destroy]
+
+	root 'resumes#index'
+	
 end
